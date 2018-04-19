4ebc937c45a0332caacd63f84771dfb27fe8128f
Saneyuki Makino <a@saneyuki-no-macbook-ea.local>
2018-04-19 23:08:50 +0900

    First commit of LameMemo

---
+++ LamesMemo/TestCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  TestCollectionReusableView.swift
+//  LamesMemo
+//
+//  Created by Saneyuki Makino on 2018/04/19.
+//  Copyright © 2018年 Lame. All rights reserved.
+//
+
+import UIKit
+
+class TestCollectionReusableView: UICollectionReusableView {
+    var testLabel:UILabel = UILabel()
+}
---
+++ LamesMemo/TestCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TestCollectionViewCell.swift
+//  LamesMemo
+//
+//  Created by Saneyuki Makino on 2018/04/19.
+//  Copyright © 2018年 Lame. All rights reserved.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    var testLabel:UILabel = UILabel()
+}
